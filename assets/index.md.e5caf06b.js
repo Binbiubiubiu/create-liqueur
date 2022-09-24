f98080bf496c976ce08d460b7d3528b0dfe1b0e1
Binbiubiubiu <2632003786@qq.com>
2022-09-24 14:09:50 +0800

    Updates

---
+++ assets/index.md.e5caf06b.js
@@ -0,0 +1,11 @@
+import{_ as e,c as a,o as n,a as s}from"./app.5fd5edf9.js";const m=JSON.parse('{"title":"Liqueur","description":"","frontmatter":{},"headers":[{"level":2,"title":"Introduction","slug":"introduction","link":"#introduction","children":[]},{"level":2,"title":"Install","slug":"install","link":"#install","children":[]},{"level":2,"title":"LICENSE","slug":"license","link":"#license","children":[]}],"relativePath":"index.md"}'),l={name:"index.md"},t=s(`<h1 id="liqueur" tabindex="-1">Liqueur <a class="header-anchor" href="#liqueur" aria-hidden="true">#</a></h1><p><a href="https://npmjs.org/package/create-liqueur" target="_blank" rel="noreferrer"><img src="https://img.shields.io/npm/v/create-liqueur.svg?style=flat" alt="NPM version"></a></p><p><a href="https://npmjs.org/package/create-liqueur" target="_blank" rel="noreferrer"><img src="http://img.shields.io/npm/dm/create-liqueur.svg?style=flat" alt="NPM downloads"></a></p><h2 id="introduction" tabindex="-1">Introduction <a class="header-anchor" href="#introduction" aria-hidden="true">#</a></h2><p>An npm package generator</p><h2 id="install" tabindex="-1">Install <a class="header-anchor" href="#install" aria-hidden="true">#</a></h2><div class="language-sh"><button class="copy"></button><span class="lang">sh</span><pre><code><span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">// Use npm</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ npm create liqueur my-package</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">// Use yarn</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ yarn create liqueur my-package</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">// Use pnpm</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ pnpm create liqueur my-package</span></span>
+<span class="line"></span>
+<span class="line"></span></code></pre></div><h2 id="license" tabindex="-1">LICENSE <a class="header-anchor" href="#license" aria-hidden="true">#</a></h2><p>MIT</p>`,9),r=[t];function i(p,c,o,d,u,h){return n(),a("div",null,r)}const g=e(l,[["render",i]]);export{m as __pageData,g as default};
